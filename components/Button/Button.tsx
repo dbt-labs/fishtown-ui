d7b2d3dd12ba71b9e7413ae6b5f87e8c606ec09a
Andrew Sweet <andrewmsweet@gmail.com>
2021-08-13 11:01:23 -0400

    Initial button changes

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,8 +1,12 @@
 import * as React from 'react';
 import cx from 'classnames';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { Icon } from 'index';
 
 export interface ButtonProps {
   size?: 'small' | 'medium' | 'large';
+  buttonStyle?: 'solid' | 'border-only';
+  icon?: string | IconDefinition;
   isDisabled?: boolean;
   title?: string;
   className?: string;
@@ -11,6 +15,8 @@ export interface ButtonProps {
 
 export const Button: React.FC<ButtonProps> = ({
   size = 'medium',
+  buttonStyle = 'solid',
+  icon,
   isDisabled,
   title,
   className,
@@ -20,14 +26,20 @@ export const Button: React.FC<ButtonProps> = ({
   const isSmall = size === 'small';
   const isMedium = size === 'medium';
   const isLarge = size === 'large';
+  const isSolidButton = buttonStyle === 'solid';
+  const isOutlineButton = buttonStyle === 'border-only';
   const classNames = cx(
     {
       'tw-py-1 tw-px-2': isSmall,
       'tw-py-2 tw-px-4': isMedium,
       'tw-py-4 tw-px-8': isLarge,
     },
-    'tw-bg-teal-500',
-    'tw-text-white',
+    {
+      'tw-bg-teal-500 tw-text-white hover:tw-bg-teal-700 disabled:tw-bg-gray-500': isSolidButton,
+      'tw-text-teal-100 tw-border-teal-100': isOutlineButton,
+      'tw-box-border tw-border hover:tw-border-2': isOutlineButton,
+      'disabled:hover:tw-border disabled:tw-text-gray-500 disabled:tw-border-gray-500': isOutlineButton,
+    },
     'tw-transition-colors',
     'tw-duration-200',
     'tw-font-bold',
@@ -37,7 +49,7 @@ export const Button: React.FC<ButtonProps> = ({
     'hover:tw-bg-teal-700',
     'disabled:tw-bg-gray-500',
     'disabled:tw-cursor-not-allowed',
-    className,
+    className
   );
 
   return (
@@ -48,6 +60,7 @@ export const Button: React.FC<ButtonProps> = ({
       className={classNames}
       {...props}
     >
+      {icon && <Icon icon={icon}></Icon>}
       {children}
     </button>
   );
